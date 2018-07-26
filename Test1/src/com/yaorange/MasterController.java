9a9dc214374c738eb74049378bdff45defeb4495
mkurisuuuu <40307984+mkurisuuuu@users.noreply.github.com>
2018-07-26 22:31:46 +0800

    Update MasterController.java
    1

--- Test1/src/com/yaorange/MasterController.java
+++ Test1/src/com/yaorange/MasterController.java
@@ -3,9 +3,5 @@ package com.yaorange;
 public class MasterController {
 	public static void main(String[] args) {
 		System.out.println("Master");
-<<<<<<< HEAD
-		//123456
-=======
-		//123
->>>>>>> f3bb64a1bc1e90a95fee461be7bae5aca489bb95
+
 }
